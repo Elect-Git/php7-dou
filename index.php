ab0beb20134232480bb92cfe1ebb4fea89c6808b
Brunin <bruno.matheaco@gmail.com>
2020-04-25 15:25:36 -0300

    Criamos a classe usuario

--- index.php
+++ index.php
@@ -1,11 +1,19 @@
 <?php 
 
-require_once("config.php");
+require_once("config.php");//chamar o id que eu pedir...
 
-$sql = new Sql();
+$yasmin = new Usuario();
+
+$yasmin->loadById(1);//...aqui
+
+echo $yasmin;
+
+
+
+/*$sql = new Sql();
 
 $usuarios = $sql->select("SELECT *FROM tb_usuarios");
 
-echo json_encode($usuarios);
+echo json_encode($usuarios);        <--- para mostrar toda a lista*/
 
  ?>
